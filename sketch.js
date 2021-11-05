830eb0695bfcdd519e6275fa5a023f36829eb8cd
Gustavo Rizzo <grizzo.albu@gmail.com>
2021-11-05 00:29:33 -0300

    Create mesh between control points

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   p_ancor1 = createVector(100, 300);
   p_control1 = createVector(200, 100)
-  p_control2 = createVector(500, 400);
+  p_control2 = createVector(500, 500);
   p_ancor2= createVector(700, 300);
 }
 
@@ -21,13 +21,43 @@ function draw() {
   point(p_control2);
   point(p_ancor2);  
 
-  //draw bezier curve
-  strokeWeight(4);
+  //draw bezier curve as a sum of lines
+  let parts = 30;
+  let delta = 1/parts;
+  stroke(255);
   noFill();
-  bezier(p_ancor1.x, p_ancor1.y, p_control1.x, p_control1.y, p_control2.x, p_control2.y, p_ancor2.x, p_ancor2.y);
+  beginShape();
+  for(let t=0; t<=1.001; t+=delta) {
+    vectf = cubic(t, p_ancor1, p_control1, p_control2, p_ancor2);
 
-  //draw controls lines
-  strokeWeight(2);
-  line(p_ancor1.x, p_ancor1.y, p_control1.x, p_control1.y);
-  line(p_ancor2.x, p_ancor2.y, p_control2.x, p_control2.y);
+    //print curve
+    strokeWeight(4)
+    vertex(vectf.x, vectf.y);
+  }
+  endShape();
+}
+
+function lerp2D(t, vector1 , vector2) {
+  let x = lerp(vector1.x, vector2.x, t);
+  let y = lerp(vector1.y, vector2.y, t);
+  return createVector(x, y);
+}
+
+function quadratic(t, p0, p1 , p2) {
+  let vect1 = lerp2D(t, p0 , p1);
+  let vect2 = lerp2D(t, p1 , p2);
+  let vectf = lerp2D(t, vect1 , vect2);
+
+  //print mesh
+  strokeWeight(1)
+  line(vect1.x, vect1.y , vect2.x, vect2.y);
+
+  return vectf
+}
+
+function cubic(t, p0, p1, p2, p3) {
+  let vect1 = quadratic(t, p0 , p1, p2);
+  let vect2 = quadratic(t, p1 , p2, p3);
+  let vectf = lerp2D(t, vect1 , vect2);
+  return vectf
 }
